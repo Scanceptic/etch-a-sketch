87f4ba80220cf429ee0d5e10fe35423abfcfa81a
Nathan <urwin.nathan@gmail.com>
2024-06-23 16:43:40 +0100

    Replace HTML etch squares with DOM

---
+++ script.js
@@ -0,0 +1,12 @@
+const etch = document.querySelector("#etch");
+
+const makeSquare = function (num) {
+    let square = document.createElement("div");
+    square.className = "square";
+    square.textContent = "Square " + num;
+    return square;
+}
+
+for (let i=1; i <= 16; i++) {
+    etch.appendChild(makeSquare(i));
+}
