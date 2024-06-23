70d67e11550a416a04e13ea8f04adb6db7728651
Nathan <urwin.nathan@gmail.com>
2024-06-23 18:18:59 +0100

    Complete new grid sizing + header/footer styling

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const etch = document.querySelector("#etch");
 
-const makeSquare = function (num) {
+const makeSquare = function (num, sideLength) {
     let square = document.createElement("div");
     square.className = "square";
     square.id = "square"+num;
-    square.textContent = "Square " + num;
-    let color = `rgb(255 ${num} 255)`;
-    square.style.backgroundColor = color;
+    if (sideLength !== undefined && sideLength <= 100) {
+        square.style.width = `${100/sideLength}%`;
+    } else {
+        square.style.width = `${100/16}%`;
+    }
+    //let color = `rgb(255 ${num} 255)`;
+    //square.style.backgroundColor = color;
     square.addEventListener('mouseover', () => {
         square.style.backgroundColor = "rgb(255, 85, 150)";
     })
@@ -22,3 +26,22 @@ const makeSquare = function (num) {
 for (let i=1; i <= 16*16; i++) {
     etch.appendChild(makeSquare(i));
 }
+
+const button = document.querySelector("#newsquare");
+button.addEventListener("click", () => {
+    const promise1 = new Promise((resolve, reject) => {
+        resolve(sideLength = prompt("How many squares per side? (Maximum 100)"));
+    });
+    promise1.then((sideLength) => {
+        if (sideLength <= 100 && sideLength > 0) {
+            while (etch.firstChild) {
+            etch.removeChild(etch.firstChild);
+            };
+            for (let i=1; i <= sideLength*sideLength; i++) {
+                etch.appendChild(makeSquare(i, sideLength));
+            };
+        } else {
+            alert("Invalid side length");
+        }
+    }); 
+});
