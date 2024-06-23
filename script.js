6bbd5758606d0564d9e101b65f0c71a6b8256557
Nathan <urwin.nathan@gmail.com>
2024-06-23 17:00:35 +0100

    Add nice colour gradient default

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const etch = document.querySelector("#etch");
 const makeSquare = function (num) {
     let square = document.createElement("div");
     square.className = "square";
+    square.id = "square"+num;
     square.textContent = "Square " + num;
+    let color = `rgb(255,${num},255)`;
+    square.style.backgroundColor = color;
     return square;
 }
 
